fcf935e6d098377ef4fcb97d5f130c1db9295a76
Sabbir Hossen <Sabbir Hossen>
2025-02-09 12:50:26 -1200

    added new

--- Assertion.ts
+++ Assertion.ts
@@ -4,3 +4,18 @@ anything = "next level hero";
 anything = 333;
 
 (anything as number)
+
+const kgToGm = (value: string | number): string | number | undefined => {
+  if (typeof value === 'string') {
+
+    const convertedValue = parseFloat(value) * 1000
+    return `The converted value is : ${convertedValue}`
+  }
+  if (typeof value === 'number') {
+    return value * 1000
+  }
+
+}
+
+const result1 = kgToGm(1000) as number
+const result2 = kgToGm("1000") as string
